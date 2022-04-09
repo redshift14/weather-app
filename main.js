7b94155db5e05312af0515590703f2e9a667bf80
redshift14 <anasarif.mi@gmail.com>
2022-04-09 19:11:46 +0100

    Handle Browser Not Allowing Location

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// const appId = '2fb188d6ac205a6a8ce5b285fd61c7a1'
-// const appImageId = 'vjJAL-7GoCmirkeZLZX9NCQ1gHgfJdq3CQTO-2SiDfE'
-
 import {UNSPLASH_API_KEY as appImageId, WEATHER_API_KEY as appId} from './keys.js'
 
 const app = document.getElementById('app')
@@ -98,9 +95,11 @@ const searchOtherCity = () => {
                 return response.json()
             })
             .then(imageData => {
-                let imageIndex = Math.floor(Math.random() * imageData.results.length - 1)
-                let imageSrc = imageData.results[imageIndex].urls.regular
-                document.body.style.backgroundImage = `url(${imageSrc})`
+                if (imageData) {
+                    let imageIndex = Math.floor(Math.random() * imageData.results.length - 1)
+                    let imageSrc = imageData.results[imageIndex].urls.regular
+                    document.body.style.backgroundImage = `url(${imageSrc})`
+                }
                 setHtmlBody(data)
                 document.getElementById('search-city-btn').addEventListener('click', searchOtherCity)
                 loader.classList.add('hidden')
@@ -116,33 +115,87 @@ const searchOtherCity = () => {
 window.addEventListener('load' , () => {
     let long;
     let lat;
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${appId}&units=metric`;
-            fetch(api).then(response => {
-                if (response.ok) { 
-                    return response.json();
-                }
-                throw new Error('Error')
-            }).then(data => {
-                const api = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${data.name.replace(/\s/g, '&')}&w=${window.innerWidth}&h=${window.innerHeight}&client_id=${appImageId}`
-                fetch(api)
-                    .then(response => {
-                        return response.json()
-                    })
-                    .then(imageData => {
+    console.log(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(position => {
+        long = position.coords.longitude;
+        lat = position.coords.latitude;
+        const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${appId}&units=metric`;
+        fetch(api).then(response => {
+            if (response.ok) { 
+                return response.json();
+            }
+            throw new Error('Error')
+        }).then(data => {
+            const api = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${data.name.replace(/\s/g, '&')}&w=${window.innerWidth}&h=${window.innerHeight}&client_id=${appImageId}`
+            fetch(api)
+                .then(response => {
+                    return response.json()
+                })
+                .then(imageData => {
+                    if (imageData.results.length > 0) {                         
                         let imageIndex = Math.floor(Math.random() * imageData.results.length - 1)
                         let imageSrc = imageData.results[imageIndex].urls.regular
                         document.body.style.backgroundImage = `url(${imageSrc})`
-                        setHtmlBody(data)
-                        document.getElementById('search-city-btn').addEventListener('click', searchOtherCity)
-                        loader.classList.add('hidden')
-                    })
-            }).catch((err) => {
-                console.log(err.message)
-            })
-        });
-    }
+                        
+                    }
+                    setHtmlBody(data)
+                    document.getElementById('search-city-btn').addEventListener('click', searchOtherCity)
+                    loader.classList.add('hidden')
+                })
+        }).catch((err) => {
+            console.log(err.message)
+        })
+    }, () => {
+        loader.classList.add('hidden')
+        app.innerHTML = `
+        <section class="left-col">
+            <h1>Temp</h1>
+            <div class="city-and-date">
+                <h2>city</h2>
+                <p>date</p>
+            </div>
+            <div class="weather-container">
+                <img id="icon">
+                <p id="weather">weather</p>
+            </div>
+        </section>
+        <section class="right-col">
+            <h3>Temperature Details</h3>
+            <div class="details">
+                <p>Feels Like</p>
+                <p id="feels like"></p>
+                <p>Clouds</p>
+                <p id="cloud-percent"> %</p>
+            </div>
+            <hr>
+            <h3>Weather Details</h3>
+            <div class="details">
+                <p>Humidity</p>
+                <p id="humidity">%</p>
+                <p>Wind</p>
+                <p id="wind">m/s</p>
+                <p>Visibility</p>
+                <p id="visibility"> m</p>
+                <p>Pressure</p>
+                <p id="pressure"> hPa</p>
+            </div>
+            <hr>
+            <h3>Sunrise & Sunset</h3>
+            <div class="details">
+                <p>Sunrise</p>
+                <p id="sunrise"></p>
+                <p>Sunset</p>
+                <p id="sunset"></p>
+            </div>
+            <hr>
+            <h3>Search New Location</h3>
+            <div class="input">
+                <input type="text" id="city-to-search" placeholder="Type a New Location">
+                <button id="search-city-btn"><img src="./icons/search-white.svg"/></button>
+            </div>
+            <p id="error-msg"></p>
+        </section>
+        `
+        document.getElementById('search-city-btn').addEventListener('click', searchOtherCity)
+    });
 })
